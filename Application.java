a8ba15db21c75ec1ae56b6eb31c32471a992f99e
Ole Pöschl <62295828+olepoeschl@users.noreply.github.com>
2025-07-27 20:22:03 +0200

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,8 @@
+/**
+ * The one and only class of this mock project.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Upme");
+    }
+}
